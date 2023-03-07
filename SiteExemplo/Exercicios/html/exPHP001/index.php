93d8e4c2cba83c4ca81a4120600148b0281b2ea4
fsilva-gesi <123658518+fsilva-gesi@users.noreply.github.com>
2023-03-07 09:41:48 +0000

    Alteradia7

---
+++ SiteExemplo/Exercicios/html/exPHP001/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Exemplo de PHP</h1>
+    <?php 
+        <h1>Exemplo PHP</h1>
+        date_default_timezone_set("Europe/Lisboa");
+        echo "Hoje é dia " . date("d/M/Y");
+        echo " e a hora é " . date("G:i:s");
+    ?>
+</body>
+</html>
