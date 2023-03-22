fb9374f04322a936ea55870581a8c7349c6a0cab
fsilva-gesi <123658518+fsilva-gesi@users.noreply.github.com>
2023-03-22 00:40:30 +0000

    img001

---
+++ SiteExemplo/Exercicios/html/js003/script.js
@@ -0,0 +1,22 @@
+var bt1 = window.document.getElementById("bt1")
+var bt2 = window.document.getElementById("bt2")
+var bt3 = window.document.getElementById("bt3")
+var img = document.getElementById("img")
+
+bt1.addEventListener("click", function () { muda(1) })
+bt2.addEventListener("click", function () { muda(2) })
+bt3.addEventListener("click", function () { muda(3) })
+
+function muda(par){
+    switch (par){
+        case 1:
+            img.src ="imagens/manha.jpg"
+            break;
+        case 2:
+            img.src = "imagens/tarde.jpg"
+            break;
+        case 3:
+            img.src = "imagens/noite.jpg"
+    }
+}
+
